a2810325b43a88953d900f48155cf5acb334927e
Jeremy Teo <jeremyteo88@hotmail.com>
2021-10-23 23:11:21 +0800

    Added comments to commentsEntry.js

--- app/components/commentsEntry.js
+++ app/components/commentsEntry.js
@@ -2,14 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { ThemeConsumer } from "../contexts/theme";
 
+// Functional component for comments entry
 export function commentsEntry (comments) {
 
     const { id, by, text, time } = comments
 
+    // Creates an object with __html key to store the comments text.
     const specialText = {__html: text}
 
     var date = new Date(time)
 
+    // Text loaded is then indicated as dangerouslySetInnerHTML to load HTML tags
     return(
         <ThemeConsumer>
             {({ theme }) => (
